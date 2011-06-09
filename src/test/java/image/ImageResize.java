8e26251c033b606bf10515329fb68cf162dd05d7
feng <feng@onyx-international.com>
2011-06-09 08:26:01 +0800

    update lucene to 3.2

--- src/test/java/image/ImageResize.java
+++ src/test/java/image/ImageResize.java
@@ -1,5 +1,6 @@
 package image;
 
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,14 +9,14 @@ import javax.imageio.ImageIO;
 
 import org.junit.Test;
 
-import com.thebuzzmedia.imgscalr.Scalr;
+//import com.thebuzzmedia.imgscalr.Scalr;
 
 public class ImageResize {
 
     @Test
     public void testScalrResize() throws IOException{
-        BufferedImage img = ImageIO.read(new File("/home/feng/Downloads/a.jpg"));
-        BufferedImage resizedImg = Scalr.resize(img, 100);
-        ImageIO.write(resizedImg, "png", new File("/tmp/a.png"));
+//        BufferedImage img = ImageIO.read(new File("/home/feng/Downloads/a.jpg"));
+////        BufferedImage resizedImg = Scalr.resize(img, 100);
+//        ImageIO.write(resizedImg, "png", new File("/tmp/a.png"));
     }
 }
