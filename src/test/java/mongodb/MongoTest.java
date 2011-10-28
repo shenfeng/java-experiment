341848c4bced92691c7c9daf90ac24fbe6a9f3d9
feng <feng.shen@onycloud.com>
2011-10-28 19:23:56 +0800

    add simple mongo gridfs test

--- src/test/java/mongodb/MongoTest.java
+++ src/test/java/mongodb/MongoTest.java
@@ -1,5 +1,7 @@
 package mongodb;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,8 +9,11 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.mongodb.BasicDBObject;
@@ -17,16 +22,81 @@ import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 import com.mongodb.ServerAddress;
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSInputFile;
+
+class DummyOutputStream extends OutputStream {
+    public void write(int b) throws IOException {
+    }
+}
 
 public class MongoTest {
 
     private static final String TEST_COLL = "t";
     private static final String TEST_DB = "repl2";
-    private static final int THREAD_SIZE = 40;
+    private static final int THREAD_SIZE = 20;
+    static Logger logger = Logger.getLogger(MongoTest.class);
+
+    static class MongoFSTask implements Runnable {
+
+        private static final AtomicLong idGen = new AtomicLong(1);
+
+        private GridFS fs;
+        private RandBytes rb = new RandBytes();
+        private long rand = 0;
+        private final Random r = new Random();
+        private final long stopTime;
+
+        public MongoFSTask(GridFS fs, long duration) {
+            this.fs = fs;
+            stopTime = duration + System.currentTimeMillis();
+        }
+
+        public void run() {
+            logger.info("start");
+            while (true) {
+                long id = idGen.getAndIncrement();
+                GridFSInputFile file = fs
+                        .createFile(rb.get(r.nextInt(500) + 30));
+                file.setFilename("id-" + id);
+                file.save();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(r.nextInt(100));
+                } catch (InterruptedException ignore) {
+                    logger.error(ignore.getMessage(), ignore);
+                }
+
+                if (id % 5 == 0) {
+                    try {
+                        rand = r.nextInt() % id;
+                    } catch (Exception e1) {
+
+                        e1.printStackTrace();
+                    }
+                    String filename = "id-" + rand;
+                    GridFSDBFile f = fs.findOne(filename);
+                    if (f != null) {
+                        logger.info("find file " + filename);
+                        try {
+                            IOUtils.copy(f.getInputStream(),
+                                    new DummyOutputStream());
+                        } catch (IOException e) {
+                            logger.error(e.getMessage(), e);
+                        }
+                    } else {
+                        logger.info("fail to find file " + filename + ", " + id);
+                    }
+                }
+
+                if (stopTime < System.currentTimeMillis())
+                    break;
+            }
+        }
+    }
 
     private static class MongoTask implements Runnable {
 
-        private static Logger logger = Logger.getLogger(MongoTest.class);
         private final long start;
         private final long maxTime;
         private final DBCollection coll;
@@ -83,21 +153,41 @@ public class MongoTest {
 
     }
 
-    @Test
-    public void testReplication() throws UnknownHostException,
-            InterruptedException {
+    DBCollection coll;
+    GridFS fs;
 
+    @Before
+    public void setup() throws UnknownHostException {
         List<ServerAddress> addrs = new ArrayList<ServerAddress>();
         // addrs.add(new ServerAddress("192.168.0.150"));
         // addrs.add(new ServerAddress("192.168.0.12"));
-        addrs.add(new ServerAddress("192.168.0.101"));
+        addrs.add(new ServerAddress("127.0.0.1"));
         Mongo mongo = new Mongo(addrs);
         mongo.slaveOk();
 
         mongo.dropDatabase(TEST_DB);
         DB db = mongo.getDB(TEST_DB);
-        DBCollection coll = db.getCollection(TEST_COLL);
+        coll = db.getCollection(TEST_COLL);
+        fs = new GridFS(db);
 
+    }
+
+    @Test
+    public void testMongoFS() throws InterruptedException {
+        ExecutorService exec = Executors.newFixedThreadPool(THREAD_SIZE);
+
+        final long duration = TimeUnit.MINUTES.toMillis(5);
+
+        for (int i = 0; i < THREAD_SIZE; ++i) {
+            exec.submit(new MongoFSTask(fs, duration));
+        }
+        exec.shutdown();
+        exec.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testReplication() throws UnknownHostException,
+            InterruptedException {
         ExecutorService exec = Executors.newFixedThreadPool(THREAD_SIZE);
 
         long start = System.currentTimeMillis();
@@ -109,5 +199,4 @@ public class MongoTest {
         exec.shutdown();
         exec.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
     }
-
 }
---
+++ src/test/java/mongodb/RandBytes.java
@@ -0,0 +1,27 @@
+package mongodb;
+import java.util.Random;
+
+public class RandBytes {
+
+    private int index = 0;
+    private byte[] bytes = new byte[1024 * 1024];
+
+    public RandBytes() {
+        new Random().nextBytes(bytes);
+    }
+
+    public byte[] get(int k) {
+        k = 1024 * k;
+
+        byte[] ret = new byte[k];
+
+        for (int i = 0; i < k; ++i) {
+            index++;
+            if (index == bytes.length)
+                index = 0;
+
+            ret[i] = bytes[index];
+        }
+        return ret;
+    }
+}
